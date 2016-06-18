a50b5ca34a28683289ddfc356583c24f445ecbb6
Giuliano de Franchis <giuliano.defranchis@gmail.com>
2016-06-18 23:16:17 +0200

    la fonction fait une carte du monde centrée sur un point fourni en argument

--- master.R
+++ master.R
@@ -50,19 +50,21 @@ world.df <- world.df[order(world.df$ordre),]
 # les pays qui n'ont pas encore le prélèvementà la source ont une date d'entrée du PAS qui vaut 9999
 world.df$annee.pas[is.na(world.df$annee.pas)==TRUE] <- 9999
 
-# calcul des centroides des pays
-coco <- cbind(worldMap@data,getSpPPolygonsLabptSlots(worldMap))
+# Colorier ou non (0 ou 1)
+world.df$PAS <- 0
+world.df$PAS[which(world.df$annee.pas < year)] <- 1
+world.df$PAS <- factor(world.df$PAS)
+
+# ajout des centroides de la région principale de chaque pays à la base de données ayant adopté le PAS
+world_data <- cbind(worldMap@data,getSpPPolygonsLabptSlots(worldMap))
+world_data_pas <- merge(world_data,df.pas,by.x = c("ADMIN"),by.y=c("region"))
+world_data_pas <- world_data_pas[order(world_data_pas$annee.pas),]
+
 
 # Fonction produisant le graphique ====
-
-# La fonction qui produit le graphique avec un angle donné, et colorie
+# La fonction qui produit le graphique centré sur une coordonnée passée en argument
 # les pays qui ont le prélèvement à la source 
-rotateMap <- function(angle, year){
-  # Colorier ou non (0 ou 1)
-  world.df$PAS <- 0
-  world.df$PAS[which(world.df$annee.pas < year)] <- 1
-  world.df$PAS <- factor(world.df$PAS)
-  
+rotateMap <- function(coord){
   # Le graphique. C'est un ggplot classique mais la fonction "coord_map" fait
   # la projection
   p <- ggplot() 
@@ -70,14 +72,17 @@ rotateMap <- function(angle, year){
   p <- p + geom_polygon(data = world.df, aes(x = long, y = lat, group = group, fill = PAS))
   p <- p + scale_y_continuous(breaks = (-2:2) * 20) 
   p <- p + scale_x_continuous(breaks = (-4:4) * 15) 
-  p <- p + coord_map("ortho", orientation=c(61, angle, 0)) 
+  p <- p + coord_map("ortho", orientation=c(coord[1], coord[2], 0)) 
   p <- p + scale_fill_manual(breaks = c(0, 1),values = c("grey80",  "red"),guide = FALSE)
   p <- p + theme(panel.grid.major = element_line(colour = "black"),panel.grid.minor = element_line(colour = "black"))
   p
 }
 
-undebug(rotateMap)
-rotateMap(0,1950)
+
+# dessin d'une carte par pays ayant adopté le PAS, par ordre d'adoption du PAS
+
+coord <- world_data[3,c(50,51)]
+rotateMap(coord)
 # Création de l'animation avec le package animation ====
 
 # # Choix des années et des rotations
